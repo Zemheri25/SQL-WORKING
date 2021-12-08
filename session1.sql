7998ada730b0c045390ce99375a03430cf8e867d
Zemheri25 <omerzemheri14@gmail.com>
2021-12-09 00:35:11 +0300

    first commit

---
+++ session1.sql
@@ -0,0 +1,25 @@
+SELECT Name
+FROM tracks;
+
+SELECT Name, Composer
+FROM tracks;
+
+SELECT *
+FROM tracks;
+
+SELECT DISTINCT Composer
+FROM tracks;
+
+SELECT DISTINCT AlbumId, MediaTypeId
+FROM tracks;
+
+SELECT name
+FROM tracks
+where Composer = 'Jimi Hendrix';
+
+SELECT *
+FROM invoices
+WHERE total > 10;
+
+SELECT *
+FROM invoices;
